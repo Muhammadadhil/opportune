6c0a336314cbf039acb186ae3ef43be9b2cf1e27
muhammad adhil <muhammadadhil934@gmail.com>
2024-09-30 00:14:22 +0530

    started google-auth

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
-            <GoogleOAuthProvider clientId={import.meta.VITE_GOOGLE_CLIENT_ID}>
+            <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
                 <Provider store={store}>
                     {/* <PersistGate loading={null} persistor={persistor}> */}
                     <App />
