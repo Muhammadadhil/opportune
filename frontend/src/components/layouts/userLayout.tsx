6f11cd0f7c30d722b439f55fb98bd16383036227
muhammad adhil <muhammadadhil934@gmail.com>
2024-10-09 22:31:19 +0530

    started freelancer Details page

---
+++ frontend/src/components/layouts/userLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar
+
+const userLayout = () => {
+  return (
+  <div className="w-full xl:w-[1280px] mx-auto relative pt-6 min-h-dvh max-md:w-screen">
+    
+  </div>
+  );
+}
+
+export default userLayout 
