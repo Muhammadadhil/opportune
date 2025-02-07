256953bada60eeae086953a8a0adaa0ce5648438
muhammad adhil <muhammadadhil934@gmail.com>
2025-02-07 18:09:35 +0530

    changes made in notification socket hooks

--- frontend/src/hooks/socket/useSocket.ts
+++ frontend/src/hooks/socket/useSocket.ts
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 import { Socket } from "socket.io-client";
 import createSocketConnection from "@/utils/socketConnection";
 
-console.log("import.meta.env.VITE_NODE_ENV::", import.meta.env.VITE_NODE_ENV);
 
-const SOCKET_URL = import.meta.env.VITE_NODE_ENV === "production" ? `wss://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}` : `ws://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}`;
+const env = import.meta.env.VITE_NODE_ENV.trim();
+console.log("import.meta.env.VITE_NODE_ENV::", env);
+const SOCKET_URL = env == "production" ? `wss://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}` : `ws://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}`;
+console.log("Comparison result:", import.meta.env.VITE_NODE_ENV == "production");
 console.log("socket url for notification:", SOCKET_URL);
     
 export const useSocket = (userId?: string, userRole?: string) => {
