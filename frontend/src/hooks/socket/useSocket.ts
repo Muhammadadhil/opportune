e6495ee187d5d7d419ed45f0b6af8f0c1de20c7c
muhammad adhil <muhammadadhil934@gmail.com>
2025-02-07 17:53:54 +0530

    changes made in notification socket hooks

--- frontend/src/hooks/socket/useSocket.ts
+++ frontend/src/hooks/socket/useSocket.ts
@@ -4,7 +4,7 @@ import createSocketConnection from "@/utils/socketConnection";
 
 console.log("import.meta.env.VITE_NODE_ENV::", import.meta.env.VITE_NODE_ENV);
 
-const SOCKET_URL = import.meta.env.VITE_NODE_ENV != "production" ? `ws://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}` : `wss://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}`;
+const SOCKET_URL = import.meta.env.VITE_NODE_ENV === "production" ? `wss://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}` : `ws://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}`;
 console.log("socket url for notification:", SOCKET_URL);
     
 export const useSocket = (userId?: string, userRole?: string) => {
