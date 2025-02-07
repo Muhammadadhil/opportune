352132d3ebde6bd32fecd71656a0b43882b6251c
muhammad adhil <muhammadadhil934@gmail.com>
2025-02-07 17:17:18 +0530

    changes made in socket hook

--- frontend/src/hooks/socket/useSocket.ts
+++ frontend/src/hooks/socket/useSocket.ts
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 import { Socket } from "socket.io-client";
 import createSocketConnection from "@/utils/socketConnection";
 
-const SOCKET_URL = import.meta.env.NODE_ENV === "production" ? `wss://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}` : `ws://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}`;
+console.log("import.meta.env.NODE_ENV::", import.meta.env.NODE_ENV);
+
+const SOCKET_URL = import.meta.env.NODE_ENV != "production" ? `ws://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}` : `wss://${import.meta.env.VITE_NOTIFICATION_SOCKET_URL}`;
 console.log("socket url for notification:", SOCKET_URL);
     
 export const useSocket = (userId?: string, userRole?: string) => {
